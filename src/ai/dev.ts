a8b40917bc1788878af11fa0671675f99d3dbeea
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-09-06 15:05:44 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/filter-false-complaints.ts';
-import '@/ai/flows/calculate-urgency-score.ts';
-import '@/ai/flows/translate-text.ts';
-import '@/ai/flows/analyze-issue.ts';
-import '@/ai/flows/parse-command.ts';
